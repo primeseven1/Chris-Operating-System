0e68b2c798133364fcd8e55436d508bf7ebfc12f
primeseven1 <cgfandrich@outlook.com>
2023-04-28 16:18:53 -0700

    add some more functionality to the operating system

---
+++ src/user/shell/input/user_input.c
@@ -0,0 +1,66 @@
+#include <libc/stdio.h>
+#include <libc/stdlib.h>
+#include <libc/string.h>
+
+const char* userInput()
+{
+    // The X of the cursor location
+    // TODO: Improve this later
+    
+    // Max line number (79) + null terminator
+    char* input = (char*)malloc(V_MODE_WIDTH);
+
+    if (!input)
+        return NULL;
+
+    memset(input, 0, V_MODE_WIDTH);
+    int cursorLocationX = 1;
+    static int cursorLocationY = 2;
+
+    if (cursorLocationY > 24)
+    {
+        clrscr(VGA_BLACK);
+        cursorLocationY = 1;
+        moveCursor(cursorLocationX, cursorLocationY);
+    }
+
+    printf("\n>");
+    int charsOnLine = 1;
+
+    for (;;)
+    {
+        char character = getChar();
+
+        // Handle tabs later
+        if (character == '\t')
+            continue;
+
+        if (character == '\n')
+        {
+            input[charsOnLine - 1] = '\0';
+            cursorLocationY += 3;
+            moveCursor(1, cursorLocationY);
+            return input;
+        }
+
+        else if (character == '\b')
+        {
+            if (charsOnLine < 2)
+                continue;
+
+            printf("\b \b");
+            input[charsOnLine - 1] = ' ';
+            charsOnLine--;
+            moveCursor(--cursorLocationX, cursorLocationY);
+            continue;
+        }
+
+        if (cursorLocationX > 78)
+            continue;
+
+        printf("%c", character);
+        moveCursor(++cursorLocationX, cursorLocationY);
+        input[charsOnLine - 1] = character;
+        charsOnLine++;
+    }
+}
--- src/user/shell/shell_main.c
+++ src/user/shell/shell_main.c
@@ -1,43 +1,26 @@
 #include <libc/stdio.h>
+#include <libc/stdlib.h>
+#include "./input/user_input.h"
 
 void shell_main()
 {
     cursorEnable(15, 15);
     moveCursor(1, 2);
 
-    printf("Work in progress\n\n");
-
-    // The X of the cursor location
-    // TODO: Improve this later
-    int cursorLocation = 1;
-
-    printf(">");
-    int charsOnLine = 1;
+    printf("Work in progress\n");
 
     for (;;)
     {
-        char character = getChar();
+        // Heap allocated
+        const char* input = userInput();
 
-        // Handle tabs later
-        if (character == '\n' || character == '\t')
-            continue;
-
-        if  (character == '\b')
+        if (!input)
         {
-            if (charsOnLine < 2)
-                continue;
-
-            printf("\b \b");
-            charsOnLine--;
-            moveCursor(--cursorLocation, 2);
-            continue;
+            printf("Error with allocating memory");
+            for (;;);
         }
 
-        if (cursorLocation > 78)
-            continue;
-
-        printf("%c", character);
-        moveCursor(++cursorLocation, 2);
-        charsOnLine++;
+        printf("\nYou entered:\n%s", input);
+        free(input);
     }
 }
